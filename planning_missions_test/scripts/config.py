89541c36d49a8fe8fdf40841b46ad8360eb2812e
PC <zymouse@qq.com>
2022-10-17 16:18:46 +0800

    null

--- planning_missions_test/scripts/config.py
+++ planning_missions_test/scripts/config.py
@@ -1,11 +1,10 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-import rospy
 
 script_path = os.path.dirname(os.path.abspath(__file__)) 
 
-__planningFlienName = "config/planning2.json"
+__planningFlienName = "config/normalizedOperation.json"
 planningFlie_path = script_path + "/../" + __planningFlienName
 
 planning_topic = ""
